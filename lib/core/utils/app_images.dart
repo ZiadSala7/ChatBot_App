592c63c22c2a7c8a935f1bed34b49a55622ed130
ZiadSala7 <zslah2800@gmail.com>
2024-12-06 08:48:27 +0200

    AppImages refactor

--- lib/core/utils/app_images.dart
+++ lib/core/utils/app_images.dart
@@ -3,6 +3,10 @@ class AppImages {
   static const String onboarding = 'assets/images/onboarding.png';
   static const String splash = 'assets/images/splash.png';
   static const String continueButton = 'assets/images/continue.png';
-  static const String firstViewOnboarding = 'assets/images/firstView.png';
-  static const String secondViewOnboarding = 'assets/images/secondView.png';
+  static const String chatArrow = 'assets/images/chatArrow.png';
+  static const String uploadIcon = 'assets/images/upload.png';
+  static const String muteIcon = 'assets/images/mute.png';
+  static const String chatProfile = 'assets/images/chatProf.png';
+  static const String microphone = 'assets/images/microphone.png';
+  static const String galleryIcon = 'assets/images/gallery.png';
 }
