98ddafea9ddda56b4e006c9a38ef7b434b89a869
ZiadSala7 <zslah2800@gmail.com>
2024-12-09 22:08:18 +0200

    hone cubit created

---
+++ lib/features/home/presentation/managers/cubit/home_cubit.dart
@@ -0,0 +1,6 @@
+import 'package:chatbot_app/features/home/presentation/managers/cubit/home_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class HomeCubit extends Cubit<HomeState> {
+  HomeCubit() : super(HomeInitialState());
+}
---
+++ lib/features/home/presentation/managers/cubit/home_state.dart
@@ -0,0 +1,9 @@
+abstract class HomeState {}
+
+class HomeInitialState extends HomeState {}
+
+class HomeLoadingState extends HomeState {}
+
+class HomeFailureState extends HomeState {}
+
+class HomeSuccessState extends HomeState {}
