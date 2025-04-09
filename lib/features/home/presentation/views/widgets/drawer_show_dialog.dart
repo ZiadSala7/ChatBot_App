2fde4245a614fe327b588c2de8398c9ba521f5f4
Ziad <zslah2800@gmail.com>
2025-04-09 14:10:26 +0200

    drawer and refactors

--- lib/features/home/presentation/views/widgets/drawer_show_dialog.dart
+++ lib/features/home/presentation/views/widgets/drawer_show_dialog.dart
@@ -1,42 +1,25 @@
 import 'package:flutter/material.dart';
 
 import '../../../../../core/utils/app_styles.dart';
-import 'dialog_elevated_button.dart';
+import 'dialog_buttons_section.dart';
 
 Future<dynamic> drawerShowDialog(BuildContext context) {
   return showDialog(
     context: context,
     builder: (context) {
-      return Dialog(
+      return const Dialog(
         child: SizedBox(
           height: 300,
           child: Column(
             spacing: 24,
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const Text(
+              Text(
                 'Are you sure you want to delete this chat?',
                 textAlign: TextAlign.center,
                 style: AppStyles.textStyle20ParkinsBold,
               ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    DialogElevatedButton(
-                      text: 'Delete',
-                      color: Colors.red,
-                      onPressed: () {},
-                    ),
-                    DialogElevatedButton(
-                      text: 'Cancel',
-                      color: Colors.green,
-                      onPressed: () {},
-                    ),
-                  ],
-                ),
-              )
+              DialogButtonsSection(),
             ],
           ),
         ),
