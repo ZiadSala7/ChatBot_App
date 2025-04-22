a2c7292be0cf8e37b23b5b62f0b8de112dd42656
Ziad <zslah2800@gmail.com>
2025-04-22 19:36:16 +0200

    cacheHelper to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
+import 'package:chatbot_app/constants.dart';
 import 'package:chatbot_app/core/cache/cashe_helper.dart';
 import 'package:chatbot_app/core/hive/hive_services.dart';
 import 'package:chatbot_app/open_ai_chat_app.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   HiveServices.registerAdapters();
+  await CacheHelper.init();
+  currentId = CacheHelper.sharedPreferences.getString(activeId) ?? '';
   HiveServices.init();
   CacheHelper.init();
   runApp(const OpenAIChatApp());
